f6ea53a63550e9433385853382be3a74c7a1d789
Salama Ashoush <salamaashoush@gmail.com>
2023-10-20 09:31:08 +0200

    challenge-01

---
+++ src/challenges/01/01-identity.ts
@@ -0,0 +1,20 @@
+import type { Equal, Expect } from "../../helpers";
+
+/**
+ * The `identity` function takes a value of any type
+ * and returns it. Make it generic!
+ */
+function identity(a: TODO): TODO {
+  return a;
+}
+
+// DON'T CHANGE ANYTHING BELOW THIS LINE
+let input1 = 10;
+let res1 = identity(input1);
+
+type test1 = Expect<Equal<typeof res1, number>>;
+
+let input2 = "Hello";
+let res2 = identity(input2);
+
+type test2 = Expect<Equal<typeof res2, string>>;
