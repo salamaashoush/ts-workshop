f76b8462462d5093855f5bcfc5c4ece3dd7c5a7d
Salama Ashoush <sashoush@box.com>
2024-08-12 17:23:20 +0200

    03

---
+++ src/challenges/03/01-keys.ts
@@ -0,0 +1,19 @@
+import { Equal, Expect } from '../../helpers';
+
+/**
+ * 1. implement a generic to get the union of all keys of an object type.
+ */
+type Keys<Obj> = TODO;
+
+// DO NOT CHANGE THE CODE BELOW
+type res1 = Keys<{ a: number; b: string }>;
+type test1 = Expect<Equal<res1, 'a' | 'b'>>;
+
+type res2 = Keys<{ a: number; b: string; c: unknown }>;
+type test2 = Expect<Equal<res2, 'a' | 'b' | 'c'>>;
+
+type res3 = Keys<{}>;
+type test3 = Expect<Equal<res3, never>>;
+
+type res4 = Keys<{ [K in string]: boolean }>;
+type test4 = Expect<Equal<res4, string>>;
---
+++ src/challenges/03/05-assign.ts
@@ -0,0 +1,31 @@
+import { Equal, Expect } from '../../helpers';
+
+/**
+ * Since intersections are applied recursively,
+ * how would you write an `Assign<A, B>` type-level
+ * function that matches the behavior of `{...a, ...b}`,
+ * and overrides properties of `A` with properties of `B`?
+ */
+type Assign<A, B> = TODO;
+
+const assign = <A, B>(obj1: A, obj2: B): Assign<A, B> => ({
+  ...obj1,
+  ...obj2,
+});
+
+// DO NOT CHANGE THE CODE BELOW
+// Override `id`
+type res1 = Assign<{ name: string; id: number }, { id: string }>;
+type test1 = Expect<Equal<res1, { name: string } & { id: string }>>;
+
+// Override `age` and `role`
+type res2 = Assign<{ name: string; age: string; role: string }, { age: 42; role: 'admin' }>;
+type test2 = Expect<Equal<res2, { name: string } & { age: 42; role: 'admin' }>>;
+
+// No overlap
+type res3 = Assign<{ name: string; id: number }, { age: number }>;
+type test3 = Expect<Equal<res3, { name: string; id: number } & { age: number }>>;
+
+// Using type inference from values
+const res4 = assign({ name: 'Bob', id: 4 }, { id: '3' });
+type test4 = Expect<Equal<typeof res4, { name: string } & { id: string }>>;
